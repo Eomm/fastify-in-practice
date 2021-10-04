bbbd7b3dfc2e1d91be894fd205c72890d76af33c
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-10-05 00:10:45 +0200

    configuration loader

--- server.js
+++ server.js
@@ -1,43 +1,54 @@
 'use strict'
 
 const { fastify } = require('fastify')
+const configLoader = require('./lib/config')
 
-const app = fastify({
-  logger: true
-})
+async function run () {
+  const config = await configLoader()
 
-app.register(require('fastify-mongodb'), {
-  url: process.env.MONGODB_URL || 'mongodb://localhost:27017/fastify-mongodb-example'
-})
-
-app.post('/todos', async function insertTodo (request, reply) {
-  const todo = request.body
-  const todosCollection = this.mongo.db.collection('todos')
-  const result = await todosCollection.insertOne(todo)
-  reply.code(201)
-  return { id: result.insertedId }
-})
-
-app.get('/todos', function readTodos (request, reply) {
-  const todosCollection = this.mongo.db.collection('todos')
-  return todosCollection.find().toArray()
-})
-
-app.put('/todos/:id', async function insertTodo (request, reply) {
-  const todo = request.body
-  const todosCollection = this.mongo.db.collection('todos')
-  const result = await todosCollection.updateOne({ _id: this.mongo.ObjectId(request.params.id) }, {
-    $set: {
-      done: todo.done
-    }
+  const app = fastify({
+    logger: true
   })
-  if (result.matchedCount === 0) {
-    const error = new Error('Object not found: ' + request.params.id)
-    error.status = 404
-    throw error
-  }
-  reply.code(201)
-  return { id: request.params.id }
-})
 
-app.listen(process.env.PORT || 8080)
+  app.register(require('fastify-mongodb'), config.mongo)
+
+  app.post('/todos', async function insertTodo (request, reply) {
+    const todo = request.body
+    const todosCollection = this.mongo.db.collection('todos')
+    const result = await todosCollection.insertOne(todo)
+    reply.code(201)
+    return { id: result.insertedId }
+  })
+
+  app.get('/todos', function readTodos (request, reply) {
+    const todosCollection = this.mongo.db.collection('todos')
+    return todosCollection.find().toArray()
+  })
+
+  app.put('/todos/:id', async function insertTodo (request, reply) {
+    const todo = request.body
+    const todosCollection = this.mongo.db.collection('todos')
+    const result = await todosCollection.updateOne(
+      { _id: this.mongo.ObjectId(request.params.id) },
+      {
+        $set: {
+          done: todo.done,
+          dateModified: new Date()
+        }
+      })
+    if (result.matchedCount === 0) {
+      const error = new Error('Object not found: ' + request.params.id)
+      error.status = 404
+      throw error
+    }
+    reply.code(201)
+    return { id: request.params.id }
+  })
+
+  return app.listen(config.listen)
+}
+
+run().catch(err => {
+  console.error(err)
+  process.exit(1)
+})

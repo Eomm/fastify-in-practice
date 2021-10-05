277832b2ec4016e1d31112245552ff717886642e
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-10-05 20:35:05 +0200

    todo plugin

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { fastify } = require('fastify')
-const schemas = require('./lib/schemas')
+const todoPlugin = require('./todo')
 
 module.exports = function buildApplication (config) {
   const app = fastify({
@@ -12,47 +12,7 @@ module.exports = function buildApplication (config) {
   })
 
   app.register(require('fastify-mongodb'), config.mongo)
-
-  app.post('/todos', {
-    schema: {
-      body: schemas.todoSchema
-    }
-  }, async function insertTodo (request, reply) {
-    const todo = request.body
-    const todosCollection = this.mongo.db.collection('todos')
-    const result = await todosCollection.insertOne(todo)
-    reply.code(201)
-    return { id: result.insertedId }
-  })
-
-  app.get('/todos', function readTodos (request, reply) {
-    const todosCollection = this.mongo.db.collection('todos')
-    return todosCollection.find().toArray()
-  })
-
-  app.put('/todos/:id', {
-    schema: {
-      params: schemas.todoIdSchema,
-      body: schemas.todoUpdateSchema
-    }
-  }, async function insertTodo (request, reply) {
-    const todo = request.body
-    const todosCollection = this.mongo.db.collection('todos')
-    const result = await todosCollection.updateOne(
-      { _id: this.mongo.ObjectId(request.params.id) },
-      {
-        $set: {
-          done: todo.done,
-          dateModified: new Date()
-        }
-      })
-    if (result.matchedCount === 0) {
-      const error = new Error('Object not found: ' + request.params.id)
-      error.status = 404
-      throw error
-    }
-    return { id: request.params.id }
-  })
+  app.register(todoPlugin)
 
   return app
 }
